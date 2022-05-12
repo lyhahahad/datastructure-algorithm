0664b936e6d63dd6d6393b20becf940c7ead2559
glawkdxn0328 <glawkdxn0328@naver.com>
2022-05-12 09:50:08 +0900

    circluar-list-into-two-halves

--- circular_linked_list.c
+++ circular_linked_list.c
@@ -68,3 +68,43 @@ int main()
  
     return 0;
 }
+
+/* Function to split a list (starting with head) into two lists.
+   head1_ref and head2_ref are references to head nodes of
+    the two resultant linked lists */
+void splitList(struct Node *head, struct Node **head1_ref,
+                                            struct Node **head2_ref)
+{
+  struct Node *slow_ptr = head;
+  struct Node *fast_ptr = head;
+ 
+  if(head == NULL)
+    return;
+  
+  /* If there are odd nodes in the circular list then
+     fast_ptr->next becomes head and for even nodes
+     fast_ptr->next->next becomes head */
+  while(fast_ptr->next != head &&
+         fast_ptr->next->next != head)
+  {
+     fast_ptr = fast_ptr->next->next;
+     slow_ptr = slow_ptr->next;
+  } 
+ 
+ /* If there are even elements in list then move fast_ptr */
+  if(fast_ptr->next->next == head)
+    fast_ptr = fast_ptr->next;     
+   
+  /* Set the head pointer of first half */
+  *head1_ref = head;   
+      
+  /* Set the head pointer of second half */
+  if(head->next != head)
+    *head2_ref = slow_ptr->next;
+   
+  /* Make second half circular */  
+  fast_ptr->next = slow_ptr->next;
+   
+  /* Make first half circular */  
+  slow_ptr->next = head;      
+}
