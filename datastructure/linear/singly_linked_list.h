ebd642d8971f9e4f7ab6736ab119ce41ba42040a
glawkdxn0328 <glawkdxn0328@naver.com>
2022-06-23 12:54:02 +0900

    singly_linked_list 구현

---
+++ datastructure/linear/singly_linked_list.h
@@ -0,0 +1,52 @@
+struct node
+{
+    node *next;
+    int element;
+};
+
+class singly_linked_list
+{
+    public:
+        node *head, *tail;
+
+        void prepend(value);       
+        void append(value);   
+        void appendafter(index);   
+        int pop();                
+        int popFirst();            
+        int head();                
+        int tail();                
+        void remove(index);      
+
+    void prepend(int value){
+
+    }   
+
+    void append(int value){
+
+    }
+
+    void appendafter(int index){
+
+    } 
+
+    int pop(){
+        return
+    }
+
+    int popFirst(){
+        return
+    }
+
+    int head(){
+        return
+    }
+
+    int tail(){
+        return
+    }
+    
+    void remove(int index){
+
+    }
+};
