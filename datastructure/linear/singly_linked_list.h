624ed1ac7b01207451a2732002441e8a9485c3b2
glawkdxn0328 <glawkdxn0328@naver.com>
2022-06-24 09:49:25 +0900

    singly linked list prepend, append 함수 구현

--- datastructure/linear/singly_linked_list.h
+++ datastructure/linear/singly_linked_list.h
@@ -17,15 +17,25 @@ class singly_linked_list
         int head();                
         int tail();                
         void remove(index);      
-
+    
+    // 1. new node 생성, 주소값 저장하기.
+    // 2. new node head 값으로 바꾸기.
     void prepend(int value){
-
+        newNode = {&head, value};
+        *head = &newNode;
+        return;
     }   
 
+    // 1. new node 생성, 주소값 저장하기.
+    // 2. tail의 다음값 newnode값으로 바꾸기.
+    // 3. new node tail 값으로 바꾸기.
     void append(int value){
-
+        newNode = {&head, value};
+        tail.next = &newNode;
+        *tail = &newNode;
     }
 
+    // 1.위치 찾기.
     void appendafter(int index){
 
     } 
